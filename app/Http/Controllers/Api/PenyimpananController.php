a5b5256c4bbdfc58ef1a5e290aced72265b05f5f
satriaNaufal18 <satria182002@gmail.com>
2024-05-24 14:51:23 +0700

    add insert data feature

--- app/Http/Controllers/Api/PenyimpananController.php
+++ app/Http/Controllers/Api/PenyimpananController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\PenyimpananResource;
 use App\Models\Penyimpanan;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class PenyimpananController extends Controller
 {
@@ -16,5 +17,39 @@ class PenyimpananController extends Controller
 
         // mengembalikan data dengan format json yang telah di custom ($status,$message,$resource)
         return new PenyimpananResource(true, 'List data penyimpanan', $penyimpanan);
+    }
+
+    public function store(Request $request) {
+        // validasi saat menginput data
+        $validator = Validator::make($request->all(), [
+            'isian' => 'required',
+            'nama' => 'required',
+            'no_hp' => 'required',
+            'alamat' => 'required',
+            'moto_kerja' => 'required',
+        ],[
+            'isian.required'=> 'Isian tidak boleh kosong!',
+            'nama.required'=> 'Nama tidak boleh kosong!',
+            'no_hp.required'=> 'Nomor Hp tidak boleh kosong!',
+            'alamat.required'=> 'Alamat tidak boleh kosong!',
+            'moto_kerja.required'=> 'moto kerja tidak boleh kosong!',
+        ]);
+
+        // cek validasi jika gagal
+        if ($validator->fails()) {
+            return response()->json($validator->errors(),422);
+        }
+
+        $penyimpanan = Penyimpanan::create([
+            'isian'=> $request->isian,
+            'nama'=> $request->nama,
+            'no_hp'=> $request->no_hp,
+            'alamat'=> $request->alamat,
+            'moto_kerja'=> $request->moto_kerja,
+        ]);
+
+        // mengembalikan response
+        return new PenyimpananResource(true,'Data penyimpanan baru berhasil di tambahkan', $penyimpanan);
+
     }
 }
